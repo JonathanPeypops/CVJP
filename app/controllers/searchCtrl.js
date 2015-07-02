1bfa0e1188c49cdc7cc529c7e372888f411d66bd
Jonathan Peypops <jonathanpeypops@msn.com>
2015-07-02 12:00:58 +0200

    Initial commit

---
+++ app/controllers/searchCtrl.js
@@ -0,0 +1,19 @@
+angular.module('CV.controllers', ['ngRoute'])
+    .constant('apiUrl', 'http://localhost:63342/CV/index.html')
+
+    .config(function ($routeProvider) {
+
+        $routeProvider.when('/home', {
+            templateUrl: 'app/views/home.html'
+        })
+
+        $routeProvider.when('/info', {
+            templateUrl: 'app/views/info.html'
+        })
+
+        $routeProvider.when('/diploma', {
+            templateUrl: 'app/views/diploma.html'
+        })
+
+        $routeProvider.otherwise({redirectTo: '/home'})
+    })
